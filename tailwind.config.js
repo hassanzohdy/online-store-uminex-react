c64f230388626de0fd2996f3a8b417af1ad0ca39
heba taha <amltaha866@yahoo.com>
2024-07-23 16:52:39 +0300

    colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
   ],
   prefix: "",
   theme: {
+
     container: {
       center: true,
       padding: "2rem",
@@ -30,6 +31,19 @@ module.exports = {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+      },
+      colors: {
+        bodyBg: `#f1f5f6`,
+        textBody: `#515d66`,
+        main: `#212529`,
+        secondary: `#dd3842`,
+        linkHover: `#2b38d1`,
+        scrollbar: `#8d979e`,
+        starProduct: `#ffb800`,
+        btnProductHover: `#ffffff`,
+        labelNew: `#008a00`,
+        labelOut: `#ef262c`,
+        bgStatus: `#f3f4f6`,
       },
     },
   },
