d8e5bd71ec6549428a8f352f071de0d3f236f599
heba taha <amltaha866@yahoo.com>
2024-07-23 17:35:50 +0300

    push

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,51 +1,48 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  darkMode: ["class"],
-  content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-  ],
-  prefix: "",
-  theme: {
-
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
-    extend: {
-      keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
-      colors: {
-        bodyBg: `#f1f5f6`,
-        textBody: `#515d66`,
-        main: `#212529`,
-        secondary: `#dd3842`,
-        linkHover: `#2b38d1`,
-        scrollbar: `#8d979e`,
-        starProduct: `#ffb800`,
-        btnProductHover: `#ffffff`,
-        labelNew: `#008a00`,
-        labelOut: `#ef262c`,
-        bgStatus: `#f3f4f6`,
-      },
+export const darkMode = ["class"];
+export const content = [
+  "./pages/**/*.{ts,tsx}",
+  "./components/**/*.{ts,tsx}",
+  "./app/**/*.{ts,tsx}",
+  "./src/**/*.{ts,tsx}",
+];
+export const prefix = "";
+export const theme = {
+  container: {
+    center: true,
+    padding: "2rem",
+    screens: {
+      "2xl": "1400px",
     },
   },
-  plugins: [require("tailwindcss-animate")],
-}
+  extend: {
+    keyframes: {
+      "accordion-down": {
+        from: { height: "0" },
+        to: { height: "var(--radix-accordion-content-height)" },
+      },
+      "accordion-up": {
+        from: { height: "var(--radix-accordion-content-height)" },
+        to: { height: "0" },
+      },
+    },
+    animation: {
+      "accordion-down": "accordion-down 0.2s ease-out",
+      "accordion-up": "accordion-up 0.2s ease-out",
+    },
+    colors: {
+      bodyBg: `#f1f5f6`,
+      textBody: `#515d66`,
+      main: `#212529`,
+      secondary: `#dd3842`,
+      linkHover: `#2b38d1`,
+      scrollbar: `#8d979e`,
+      starProduct: `#ffb800`,
+      btnProductHover: `#ffffff`,
+      labelNew: `#008a00`,
+      labelOut: `#ef262c`,
+      bgStatus: `#f3f4f6`,
+    },
+  },
+};
+export const plugins = [require("tailwindcss-animate")];
