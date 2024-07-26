f63092e7a4c49ba1eb6f7a2c6dca8fc4d4f0602d
Alaa Shalaby <alaashalabi011@gmail.com>
2024-07-26 05:22:58 +0200

    Add Blog Card & Feature Card to home page

--- src/apps/front-office/home/pages/HomePage/HomePage.tsx
+++ src/apps/front-office/home/pages/HomePage/HomePage.tsx
@@ -1,5 +1,12 @@
+import BlogCard from "apps/front-office/design-system/components/ui/BlogCard";
+import FeatureCard from "apps/front-office/design-system/components/ui/FeatureCard";
 import "./HomePage.css";
 
 export default function HomePage() {
-  return <>Home</>;
+  return (
+    <>
+      <BlogCard />
+      <FeatureCard />
+    </>
+  );
 }
