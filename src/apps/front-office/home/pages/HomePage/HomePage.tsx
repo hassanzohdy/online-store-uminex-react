a8feb300c06b4c008e166beadb41a3b96f8d09e3
MohamedAbdelrhman <mohamed.abdelrahman.eltahawy@gmail.com>
2024-07-29 19:12:13 +0300

    feat: added an api and tested with home page

--- src/apps/front-office/home/pages/HomePage/HomePage.tsx
+++ src/apps/front-office/home/pages/HomePage/HomePage.tsx
@@ -1,10 +1,15 @@
+import { preload } from "@mongez/react-utils";
+import { getHome } from "apps/front-office/home/services/home-service";
 import CategoryList from "../../components/CategoryList";
 import "./HomePage.css";
 
-export default function HomePage() {
+function _HomePage() {
   return (
     <>
       <CategoryList />
     </>
   );
 }
+const HomePage = preload(_HomePage, getHome);
+
+export default HomePage;
