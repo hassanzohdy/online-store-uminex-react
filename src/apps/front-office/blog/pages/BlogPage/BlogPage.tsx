163d325d3d52d14bbbf2f6dab75f9d400733a39c
Zeyad Abdullatif Abdullwahab Korkor <zayad30982025@f-eng.tanta.edu.eg>
2024-07-31 17:23:12 +0300

    feat: added blogPage

---
+++ src/apps/front-office/blog/pages/BlogPage/BlogPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function _BlogPage() {
+  return (
+    <>
+      <h1>BlogPage</h1>
+    </>
+  );
+}
+
+const BlogPage = React.memo(_BlogPage);
+export default BlogPage;
