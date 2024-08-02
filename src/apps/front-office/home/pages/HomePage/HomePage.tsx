86d23b327b856c8b6dbe63607e772d565dbea849
omar <omarcasio717@gmail.com>
2024-08-02 19:00:15 +0300

    Product Card

--- src/apps/front-office/home/pages/HomePage/HomePage.tsx
+++ src/apps/front-office/home/pages/HomePage/HomePage.tsx
@@ -1,5 +1,12 @@
+import TopSellingCard from "shared/TopSellingCard";
 import "./HomePage.css";
 
 export default function HomePage() {
-  return <>Home</>;
+  return (
+    <>
+      <div className="text-xl m-4 grid grid-cols-3">
+        <TopSellingCard />
+      </div>
+    </>
+  );
 }
