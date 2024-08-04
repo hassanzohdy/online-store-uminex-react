f50fb0b074c7cb1b4a43d398e25500ce24a9a899
heba taha <amltaha866@yahoo.com>
2024-08-04 17:13:01 +0300

    add total

--- src/apps/front-office/design-system/components/cart/components/CartPage/CartDetails/CartDetails.tsx
+++ src/apps/front-office/design-system/components/cart/components/CartPage/CartDetails/CartDetails.tsx
@@ -1,4 +1,5 @@
 import { FiMinus, FiPlus } from "react-icons/fi";
+import { RiDeleteBin5Line } from "react-icons/ri";
 
 import styles from "./styles.module.scss";
 export type CartDetailsProps = {
@@ -16,29 +17,32 @@ export default function CartDetails() {
       </div>
       <div className={styles.detailsBody}>
         <div className={styles.product}>
-          <div className={`${styles.productData} flex gap-4 items-center justify-between`}>
-<div className="flex gap-3 items-center">
-
-<div className={`${styles.productImg} w-24`}>
-              <img
-                src="//demo-uminex.myshopify.com/cdn/shop/products/products_13_1_grande.jpg?v=1672302391"
-                className="w-24"
-              />
-            </div>
-            <div className={styles.productInfo}>
-              <h3 className="text-primary font-text-sm font-semibold">
-                Dell Vostro 3888 i9 8GB 512GB
-              </h3>
-              <p className="text-primary font-text-sm font-semibold">- Win10</p>
-              <div className="priceWrapper flex gap-2">
-                <span className="text-secondary text-base font-semibold ">
-                  $900.00
-                </span>
-                <del className="text-darkGray semibold text-base">$928.00</del>
+          <div
+            className={`${styles.productData} flex gap-4 items-center justify-between`}>
+            <div className="flex gap-3 items-center">
+              <div className={`${styles.productImg} w-24`}>
+                <img
+                  src="//demo-uminex.myshopify.com/cdn/shop/products/products_13_1_grande.jpg?v=1672302391"
+                  className="w-24"
+                />
+              </div>
+              <div className={styles.productInfo}>
+                <h3 className="text-primary font-text-sm font-semibold">
+                  Dell Vostro 3888 i9 8GB 512GB
+                </h3>
+                <p className="text-primary font-text-sm font-semibold">
+                  - Win10
+                </p>
+                <div className="priceWrapper flex gap-2">
+                  <span className="text-secondary text-base font-semibold ">
+                    $900.00
+                  </span>
+                  <del className="text-darkGray semibold text-base">
+                    $928.00
+                  </del>
+                </div>
               </div>
             </div>
-</div>
-
 
             <div className={`${styles.productCounter} flex items-center gap-5`}>
               <div>
@@ -49,7 +53,14 @@ export default function CartDetails() {
                 <FiMinus />
               </div>
             </div>
-            <div className={styles.productDelete}></div>
+            <div className={`${styles.productTotal} `}>
+            <h3 className="text-primary font-text-sm font-semibold">
+            $3,600.00
+                </h3>
+            </div>
+            <div className={styles.productDelete}>
+              <RiDeleteBin5Line />
+            </div>
           </div>
         </div>
       </div>
--- src/apps/front-office/design-system/components/cart/components/CartPage/CartPage.tsx
+++ src/apps/front-office/design-system/components/cart/components/CartPage/CartPage.tsx
@@ -9,7 +9,7 @@ export default function CartPage() {
   return (
     <>
       <Helmet title="Cart Page" />
-      <div className={`${styles.cartWrapper} container grid gap-2 grid-cols-2`}>
+      <div className={`${styles.cartWrapper} container grid gap-2 grid-cols-2 bg-white`}>
         <CartDetails />
         <CartTotals />
       </div>
