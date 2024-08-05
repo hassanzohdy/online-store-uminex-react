4010ad704b6eebee76acc9b11cd50614488f0db3
heba taha <amltaha866@yahoo.com>
2024-08-05 13:26:16 +0300

    add color to body

--- src/apps/front-office/design-system/components/cart/components/CartPage/CartTotals/CartTotals.tsx
+++ src/apps/front-office/design-system/components/cart/components/CartPage/CartTotals/CartTotals.tsx
@@ -5,7 +5,12 @@ export type CartTotalsProps = {
 export default function CartTotals() {
   return (
     <div className={styles.total}>
-      <h1>CartTotals</h1>
+      <div className="progress"></div>
+      <div className="pt-3 pb-3 border-b-2 border-borderLight font-bold text-base uppercase">Cart totals</div>
+      <div className="pt-3 pb-3 border-b-2 border-borderLight text-base uppercase flex justify-between">
+        <h3>Subtotal</h3>
+        <p>$3,830.00</p>
+      </div>
     </div>
   );
 }
